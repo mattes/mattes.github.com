cdf62e2c784075d4a2abc3900c8e4cd873ad8014
mattes <matthias.kadenbach@gmail.com>
2013-03-28 18:26:51 +0100

    skype status v2

--- js/js.js
+++ js/js.js
@@ -1,5 +1,39 @@
-$(document).ready(function() {
+// get skype status
+// https://gist.github.com/mattes/5253271
+var getSkypeStatus = function(user, callback) {
+  if(!user) throw new Error('missing user');
+  var query = 'select * from skype.user.status where user="' + user + '";';
+  $.getJSON('http://query.yahooapis.com/v1/public/yql?q=' + encodeURI(query + '&format=json&env=store://datatables.org/alltableswithkeys&callback=?'), 
+  function(data){
+    if(data.query.count > 0) {
+      if(callback) return callback.call(null, data.query.results.result);
+    } else {
+      if(callback) return callback.call(null, false); 
+    }
+  });
+}
 
+// set skype status
+var setSkypeStatus = function() {
+  getSkypeStatus('mkadenbach', function(status){
+    if(!status) {
+      // try again every 5 seconds
+      window.setTimeout(setSkypeStatus, 5000);
+    }
+
+    $('#skypeStatus').removeClass('online offline away');
+    if(status.status == 2) {
+      $('#skypeStatus').addClass('online');
+    } else if(status.status >= 3 && status.status <= 7) {
+      $('#skypeStatus').addClass('away');
+    } else {
+      $('#skypeStatus').addClass('offline');
+    }
+  });
+};
+
+
+$(document).ready(function() {
 
   $('#m').attr('href', $.rotate13('znvygb:zngguvnf.xnqraonpu@tznvy.pbz')).append(
          $.rotate13('zngguvnf.xnqraonpu@tznvy.pbz'));
@@ -12,36 +46,9 @@ $(document).ready(function() {
            $.rotate13('fxlcr/zxnqraonpu'));
   }
 
-
-  // set skype status
-  var setSkypeStatus = function() {
-    $.get('http://kb-server.de/skype-status', function(data, textStatus, s) {
-      // 1 = offline
-      // 2 = online
-      // 3 = away
-      // 4 = not available
-      // 5 = Do Not Disturb
-      // 7 = skype me        
-      if(textStatus == 'success') {
-        skypeStatus = parseInt(data, 10);
-        console.log('skypeStatus:', skypeStatus);
-
-        $('#skypeStatus').removeClass('online offline away');
-        if(skypeStatus == 2) {
-          $('#skypeStatus').addClass('online');
-        } else if(skypeStatus >= 3 && skypeStatus <= 7) {
-          $('#skypeStatus').addClass('away');
-        } else {
-          $('#skypeStatus').addClass('offline');
-        }
-      }
-    }, 'jsonp');  
-  };
-  
   window.setTimeout(function() {
     setSkypeStatus();
     window.setInterval(setSkypeStatus, 1000 * 60 * 3 /* 3 minutes */);
   }, 4000);
-
 
 });
