5bbbe739fa2e71b9fa071a3c2c2fa50b63989bca
mattes <matthias.kadenbach@gmail.com>
2013-03-29 15:12:12 +0100

    updates

--- js/js.js
+++ js/js.js
@@ -17,17 +17,17 @@ var getSkypeStatus = function(user, callback) {
 var setSkypeStatus = function() {
   getSkypeStatus('mkadenbach', function(status){
     if(!status) {
-      // try again every 5 seconds
-      window.setTimeout(setSkypeStatus, 5000);
+      // try again every 4 seconds
+      window.setTimeout(setSkypeStatus, 4000);
     }
 
-    $('#skypeStatus').removeClass('online offline away');
+    $('#skypeStatus').removeClass('online offline away icon-circle');
     if(status.status == 2) {
-      $('#skypeStatus').addClass('online');
+      $('#skypeStatus').addClass('online icon-circle').attr('title', 'i am online');
     } else if(status.status >= 3 && status.status <= 7) {
-      $('#skypeStatus').addClass('away');
+      $('#skypeStatus').addClass('away icon-circle').attr('title', 'i am away');
     } else {
-      $('#skypeStatus').addClass('offline');
+      $('#skypeStatus').addClass('offline').attr('title', 'i am offline');
     }
   });
 };
@@ -38,7 +38,7 @@ $(document).ready(function() {
   $('#m').attr('href', $.rotate13('znvygb:zngguvnf.xnqraonpu@tznvy.pbz')).append(
          $.rotate13('zngguvnf.xnqraonpu@tznvy.pbz'));
 
-  if (navigator.userAgent.match(/iPhone/i)) {
+  if (navigator.userAgent.match(/iPhone|iPad/i)) {
     $('#s').attr('href', $.rotate13('fxlcr://zxnqraonpu')).append(
            $.rotate13('fxlcr/zxnqraonpu'));
   } else {
@@ -49,6 +49,6 @@ $(document).ready(function() {
   window.setTimeout(function() {
     setSkypeStatus();
     window.setInterval(setSkypeStatus, 1000 * 60 * 3 /* 3 minutes */);
-  }, 4000);
+  }, 500);
 
 });
